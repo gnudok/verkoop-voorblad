0871376cacf2b70a7e5b6474a183a03faeff6024
Matthew Ledgett <MatthewLedgett@gmail.com>
2015-04-15 16:57:45 +0200

    64 Bit

--- spec.sh
+++ spec.sh
@@ -3,10 +3,12 @@ DISKSIZE=$(dmesg |grep '\bsd'|grep GB|cut -d'(' -f2|cut -d' ' -f1)
 CPUSPEED=$(lscpu|grep MHz|cut -d' ' -f17)
 CORECOUNT=$(lscpu|grep Core|cut -d' ' -f7)
 MEMSIZE=$(free -m|grep Mem|cut -d' ' -f11)
+BITS64=$(if (lscpu|grep "CPU op-mode"|grep 64) &> /dev/null ; then echo "Ja"; else echo "Nee"; fi)
 
 cat > spec.tex <<EOF
 \newcommand{\disksize}{$DISKSIZE}
 \newcommand{\cpuspeed}{$CPUSPEED}
 \newcommand{\corecount}{$CORECOUNT}
 \newcommand{\memsize}{$MEMSIZE}
+\newcommand{\bitssixtyfour}{$BITS64}
 EOF
