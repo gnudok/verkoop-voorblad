9f0b5b20dfb550481be1728be3ae0ab3be2982f6
Matthew Ledgett <MatthewLedgett@gmail.com>
2015-04-15 16:39:52 +0200

    memsize command

--- spec.sh
+++ spec.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 DISKSIZE=$(dmesg |grep '\bsd'|grep GB|cut -d'(' -f2|cut -d' ' -f1)
 CPUSPEED=$(lscpu|grep MHz|cut -d' ' -f17)
+CORECOUNT=$(lscpu|grep Core|cut -d' ' -f7)
+MEMSIZE=$(free -m|grep Mem|cut -d' ' -f11)
 
 cat > spec.tex <<EOF
 \newcommand{\disksize}{$DISKSIZE}
 \newcommand{\cpuspeed}{$CPUSPEED}
+\newcommand{\corecount}{$CORECOUNT}
+\newcommand{\memsize}{$MEMSIZE}
 EOF
