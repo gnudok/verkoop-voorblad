71ea5acad95f69b855769d599c3905b4ca91fbca
Matthew Ledgett <MatthewLedgett@gmail.com>
2015-04-15 16:23:08 +0200

    CPU speed machine detected

--- spec.sh
+++ spec.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 DISKSIZE=$(dmesg |grep '\bsd'|grep GB|cut -d'(' -f2|cut -d' ' -f1)
+CPUSPEED=$(lscpu|grep MHz|cut -d' ' -f17)
 
 cat > spec.tex <<EOF
 \newcommand{\disksize}{$DISKSIZE}
+\newcommand{\cpuspeed}{$CPUSPEED}
 EOF
