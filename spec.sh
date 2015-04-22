95e132e3e304e753dfba7a79299aef8f4fca3334
Matthew Ledgett <MatthewLedgett@gmail.com>
2015-04-22 16:55:49 +0200

    Acceleration, wireless and HyperThreading.

--- spec.sh
+++ spec.sh
@@ -4,23 +4,26 @@ CPUSPEED=$(lscpu|grep MHz|cut -d' ' -f17)
 CORECOUNT=$(lscpu|grep Core|cut -d' ' -f7)
 MEMSIZE=$(free -m|grep Mem|cut -d' ' -f11)
 BITS64=$(if (lscpu|grep "CPU op-mode"|grep 64) &>  /dev/null ; then echo "Ja"; else echo "Nee"; fi)
-WIRELESS="Nee"
-ACCELLARATED="Nee"
-HYPERTHREADING="Nee"
-BURNER="DVD"
+WIRELESS=$(if (iwconfig 2>&1|grep IEEE) &> /dev/null; then echo Ja; else echo Nee;fi)
+ACCELLERATED=$(if (lsmod|grep nvidia) &> /dev/null; then echo Ja; else echo Nee;fi)
+HYPERTHREADING=$(if [ 2 = $(lscpu|grep Thread|cut -d' ' -f7) ]; then echo Ja ; else echo Nee; fi)
+BURNER="CD"
+HOSTNAME=$(hostname)
+DEBIAN_VERSION=$(cat /etc/debian_version)
+KERNEL_VERSION=$(uname -r)
 
 PPRICE=$(echo scale=2 \; \($CPUSPEED/3000\) \* 17|bc)
 WIDTHPRICE=$(if [ $BITS64 = "Ja" ]; then echo scale=2 \; 0.5 \* $PPRICE|bc; else echo 0; fi)
 MULTICOREPRICE=$(if [ $CORECOUNT = "2" ]; then echo scale=2 \; 0.5 \* $PPRICE |bc; else echo 0; fi)
 HYPERTHREADINGPRICE=$(if [ $HYPERTHREADING = "Ja" ]; then echo scale=2 \; 0.5 \* $PPRICE|bc; else echo 0; fi)
 WIRELESSPRICE=$(if [ $WIRELESS = "Ja" ]; then echo 5; else echo 0; fi)
-ACCELLARATEDPRICE=$(if [ $ACCELLARATED = "Ja" ]; then echo 5; else echo 0; fi)
+ACCELLERATEDPRICE=$(if [ $ACCELLERATED = "Ja" ]; then echo 5; else echo 0; fi)
 BURNERPRICE=$(if [ $BURNER = "DVD" ]; then echo 4; else echo 0; fi)
 HDPRICE=$(echo scale=2 \; \($DISKSIZE/120\) \* 11|bc)
 MEMPRICE=$(echo scale=2 \; \($MEMSIZE/512\) \* 6|bc)
 
 
-PRICE=$(echo $PPRICE + $WIDTHPRICE + $MULTICOREPRICE + $HYPERTHREADINGPRICE + $WIRELESSPRICE + $ACCELLARATEDPRICE + $HDPRICE + $MEMPRICE + $BURNERPRICE|bc)
+PRICE=$(echo $PPRICE + $WIDTHPRICE + $MULTICOREPRICE + $HYPERTHREADINGPRICE + $WIRELESSPRICE + $ACCELLERATEDPRICE + $HDPRICE + $MEMPRICE + $BURNERPRICE|bc)
 
 cat > spec.tex <<EOF
 \newcommand{\disksize}{$DISKSIZE}
@@ -29,6 +32,11 @@ cat > spec.tex <<EOF
 \newcommand{\memsize}{$MEMSIZE}
 \newcommand{\bitssixtyfour}{$BITS64}
 \newcommand{\price}{$PRICE}
+\newcommand{\wireless}{$WIRELESS}
+\newcommand{\debianversion}{$DEBIAN_VERSION}
+\newcommand{\kernelversion}{$KERNEL_VERSION}
+\newcommand{\accelerated}{$ACCELLERATED}
+\newcommand{\hyperthreading}{$HYPERTHREADING}
 EOF
 
 #echo $PPRICE
